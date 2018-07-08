d99fbeff845e2dd8d7cd721b4376b601e75e7639
Jacob Albright <jacob.albright23@gmail.com>
2018-07-08 18:44:31 -0500

    implemented dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <app-header :elements="elements" @showThis="showThis" />
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Header from '@/components/Header'
+import Complete from '@/components/Complete'
+import Incomplete from '@/components/Incomplete'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appHeader: Header,
+    appComplete: Complete,
+    appIncomplete: Incomplete,
+  },
+  data () {
+    return {
+      selectedComponent: 'appIncomplete',
+      elements: [
+        { title: 'Complete', name: 'appComplete' },
+        { title: 'Incomplete', name: 'appIncomplete' },
+      ]
+    }
+  },
+  methods: {
+    showThis (name) {
+      this.selectedComponent = name;
+      // this.elements.forEach(view => view.active = false);
+      // this.elements[index].active = true;
+    }
   }
 }
 </script>
@@ -23,6 +42,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="header">
+    <ul>
+      <li v-for="elem in elements" :key="elem.title"> 
+        <a @click="$emit('showThis', elem.name)" href="#">{{elem.title}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['elements']
+}
+</script>
+
+
+<style>
+
+#header {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  background: #4e123d;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  display: inline;
+  padding: 20px 40px;
+}
+
+a {
+  text-decoration: none;
+  color: beige;
+  font-size: 30px;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+
+</style>
+
+
